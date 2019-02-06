98784938a44245915978bb32e59bd4e850256191
Nathaniel May <nathanieldmay@gmail.com>
2019-02-06 10:35:07 -0500

    compiles. tests don't pass. saving.

--- src/main/scala/nest/syntax.scala
+++ src/main/scala/nest/syntax.scala
@@ -2,19 +2,22 @@
 
 package object syntax {
   import nest._
+  import reflect.runtime.universe.TypeTag
 
-  final implicit class NestSyntax[A](a: A) {
-    private[syntax] final case class UnfinishedABNest[C, D](nest: Nest[C, D], c: C) extends Pair[C, D] {
+  final implicit class NestSyntax[A : TypeTag](a: A) {
+    private[syntax] final case class UnfinishedABNest[C >: A, D](nest: Nest[C, D], c: C) extends Pair[C, D] {
       def >>(d: D): Nest[C, D] = NestCons(ABPair(c, d), nest)
     }
 
-    private[syntax] final case class UnfinishedBANest[C, D](nest: Nest[C, D], d: D) extends Pair[C, D] {
+    private[syntax] final case class UnfinishedBANest[C, D >: A](nest: Nest[C, D], d: D) extends Pair[C, D] {
       def >>(c: C): Nest[C, D] = NestCons(BAPair(d, c), nest)
     }
 
-    def <<[B](nest: Nest[A, B]): UnfinishedABNest[A, B] = UnfinishedABNest(nest, a)
-    def <<[B](nest: Nest[B, A]): UnfinishedBANest[B, A] = UnfinishedBANest(nest, a)
-    def <<   (nest: Nest[A, A]): UnfinishedABNest[A, A] = UnfinishedABNest(nest, a)
+    def <<[B : TypeTag](nest: Nest[A, B]): UnfinishedABNest[A, B] = UnfinishedABNest(nest, a)
+    def <<[B : TypeTag](nest: Nest[B, A]): UnfinishedBANest[B, A] = UnfinishedBANest(nest, a)
+    // TODO def <<             (nest: Nest[A, A]): UnfinishedABNest[A, A] = UnfinishedABNest(nest, a)
+    def <<[B](nest: Nest[Nothing, Nothing]): UnfinishedABNest[A, B] = UnfinishedABNest(nest, a)
+
   }
 
   final case class <<[A, B](leftOuter: A, inner: Nest[A, B]) extends Nest[A, B]
--- src/test/scala/nest/NestProperties.scala
+++ src/test/scala/nest/NestProperties.scala
@@ -29,22 +29,22 @@ object NestProperties extends Properties("Nest"){
         val n0 = Nest((a, b))
         n0 match {
           case EmptyNest               => false
-          case _ << EmptyNest >> _     => false
+          case x << EmptyNest >> y     => x == a && y == b
           case _ << (_ << _ >> _) >> _ => false
-          case x << _ >> y             => x == a && y == b
+          case _ << _ >> _             => false
         }
       }
     }
 
   property("constructor syntax works") = forAll { //TODO improve this test so all cases are hit
-    (a: Int, b: Boolean) => {
+    (a: String, b: Boolean) => { // TODO when a is of type Int Long v Int implicit conversion becomes a problem
       val n0 = Nest((a, b))
-      n0 match {
+      (n0 match {
         case EmptyNest           => EmptyNest
         case x << EmptyNest >> y => x << EmptyNest >> y
         case x << nest >> y      => x << nest >> y
         //TODO add this case :   val n3:   Nest[Boolean, String] = "hey" << (true << n0 >> "hi") >> false
-      }
+      }) == n0
     }
   }
 }
